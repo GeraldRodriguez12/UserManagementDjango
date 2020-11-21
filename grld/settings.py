1b0e843278230a87091faa2ac7392fa580975a7f
GRodriguez12 <67915353+GRodriguez12@users.noreply.github.com>
2020-11-21 23:35:59 +0800

    2nd commit
    designing login and register form

--- grld/settings.py
+++ grld/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -124,12 +124,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static'
+
+#]
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static')
 
-    ]
-
-
+]
 
 
 
